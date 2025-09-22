c93f493b10b339bb49a37ed59db03e0b074657a3
Manoj <manojvm008@gmail.com>
2025-09-22 15:52:01 +0530

    Safaricom-provider-with-auth2.0

--- src/main/java/org/fineract/messagegateway/sms/providers/impl/safaricom/SafaricomServiceProvider.java
+++ src/main/java/org/fineract/messagegateway/sms/providers/impl/safaricom/SafaricomServiceProvider.java
@@ -19,8 +19,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,10 +37,17 @@ import static org.fineract.messagegateway.constants.MessageGatewayConstants.PROV
 @Service(value = "safaricom")
 public class SafaricomServiceProvider extends Provider {
     private static final Logger logger = LoggerFactory.getLogger(SafaricomServiceProvider.class);
+    public static final String TOKEN = "TOKEN";
+    public static final String EXPIRES_AT = "EXPIRES_AT";
 
     @Autowired
     private TenantRepository tenantRepository;
     private final RestTemplate restTemplate = new RestTemplate();
+
+
+
+    private static final Map<String, Object> statcTokenMap = new HashMap<>();
+
 
     @Override
     public void sendEmail(SMSBridge smsBridgeConfig, OutboundMessages message){
@@ -75,10 +86,10 @@ public class SafaricomServiceProvider extends Provider {
     private void connectAndSendToServer(SMSBridge smsBridgeConfig, OutboundMessages message, Map<String, String> request) {
 
         String providerUrl = smsBridgeConfig.getConfigValue(PROVIDER_URL);
-        String authorization = smsBridgeConfig.getConfigValue(PROVIDER_AUTH_TOKEN);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add(HttpHeaders.AUTHORIZATION,authorization);
+        //headers.add(HttpHeaders.AUTHORIZATION,authorization);
+        headers.setBearerAuth(getAuthToken(smsBridgeConfig));
 
         HttpEntity<?> entity = new HttpEntity<Object>(request);
 
@@ -107,4 +118,51 @@ public class SafaricomServiceProvider extends Provider {
     public void publishZeebeVariable(OutboundMessages message){
 
     }
+
+
+    private String getAuthToken(SMSBridge smsBridgeConfig) {
+        logger.info("Fetching Access TOKEN");
+        String authApiEnabled = smsBridgeConfig.getConfigValue("auth_api_enabled");
+        if(!"true".equals(authApiEnabled)){
+            logger.info("Fetching Access TOKEN: authApiEnabled {}, setting static token", authApiEnabled);
+            return smsBridgeConfig.getConfigValue(PROVIDER_AUTH_TOKEN);
+        }
+        String authUserName = smsBridgeConfig.getConfigValue("auth_api_userName");
+        String authPassword = smsBridgeConfig.getConfigValue("auth_api_password");
+        String authScope = smsBridgeConfig.getConfigValue("auth_api_scope");
+        String authApiUrl = smsBridgeConfig.getConfigValue("auth_api_url");
+
+        if(statcTokenMap.get(TOKEN)!=null && System.currentTimeMillis() < (Long) statcTokenMap.get(EXPIRES_AT)){
+            logger.info("Fetching Access TOKEN:  setting previous token");
+            return (String) statcTokenMap.get(TOKEN);
+        }
+
+        logger.info("Fetching Access TOKEN:  Fetching new token");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBasicAuth(authUserName, authPassword);
+
+        URI uri = UriComponentsBuilder.fromHttpUrl(authApiUrl)
+                .queryParam("scope", authScope)
+                .build()
+                .toUri();
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("grant_type", "client_credentials");
+        HttpEntity<MultiValueMap<String, String>> requestEntity =
+                new HttpEntity<>(formData, headers);
+        ResponseEntity<OAuthTokenResponse> responseEntity = restTemplate.exchange(
+                uri,
+                HttpMethod.POST,
+                requestEntity,
+                OAuthTokenResponse.class
+        );
+
+        OAuthTokenResponse token = responseEntity.getBody();
+        statcTokenMap.put(TOKEN, token.getAccessToken());
+        statcTokenMap.put(EXPIRES_AT, System.currentTimeMillis() + (token.getExpiresIn()*1000));
+
+        return (String) statcTokenMap.get(TOKEN);
+    }
+
 }
